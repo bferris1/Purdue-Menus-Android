2cce1300910511a808e40b85ea8f8194ad9a65f9
Ben Ferris <ben.ferris1@gmail.com>
2018-03-05 23:37:02 -0500

    Add methods to support favorite management

--- app/src/main/java/com/moufee/purduemenus/repository/FavoritesRepository.java
+++ app/src/main/java/com/moufee/purduemenus/repository/FavoritesRepository.java
@@ -1,28 +1,60 @@
 package com.moufee.purduemenus.repository;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Transformations;
 
 import com.moufee.purduemenus.db.FavoriteDao;
 import com.moufee.purduemenus.menus.Favorite;
+import com.moufee.purduemenus.menus.MenuItem;
+import com.moufee.purduemenus.util.AppExecutors;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import javax.inject.Inject;
 
 /**
  * Created by Ben on 13/08/2017.
+ * Repository for Favorites
  */
 
 public class FavoritesRepository {
 
     private FavoriteDao mFavoriteDao;
+    private AppExecutors mAppExecutors;
 
     @Inject
-    public FavoritesRepository(FavoriteDao favoriteDao) {
+    public FavoritesRepository(FavoriteDao favoriteDao, AppExecutors appExecutors) {
         mFavoriteDao = favoriteDao;
+        mAppExecutors = appExecutors;
     }
+
 
     public LiveData<List<Favorite>> getFavorites() {
         return mFavoriteDao.loadAllFavorites();
+    }
+
+    public LiveData<Set<String>> getFavoriteIDSet() {
+        return Transformations.map(mFavoriteDao.getFavoriteIDs(), favoriteIDs -> {
+            if (favoriteIDs != null)
+                return new HashSet<>(favoriteIDs);
+            return new HashSet<>();
+
+        });
+    }
+
+    public void addFavorite(MenuItem item) {
+        Favorite favorite = new Favorite(item.getName(), UUID.randomUUID().toString(), item.getId(), item.isVegetarian());
+        mAppExecutors.diskIO().execute(() -> mFavoriteDao.insertFavorites(favorite));
+    }
+
+    public void removeFavorite(MenuItem item) {
+        mAppExecutors.diskIO().execute(() -> mFavoriteDao.deleteByItemID(item.getId()));
+    }
+
+    public void clearLocalFavorites() {
+        mAppExecutors.diskIO().execute(() -> mFavoriteDao.deleteAll());
     }
 }
