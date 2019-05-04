3d6f5415917bc99c7c4983d88b4c4f63ace7c5ae
Ben Ferris <ben.ferris1@gmail.com>
2019-05-04 03:03:14 -0400

    Fix ProGuard issues

--- app/src/main/java/com/moufee/purduemenus/menus/Location.kt
+++ app/src/main/java/com/moufee/purduemenus/menus/Location.kt
@@ -1,11 +1,13 @@
 package com.moufee.purduemenus.menus
 
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 /**
  * Represents a Location (usually a dining court) for which menus can be retrieved
  */
+@Keep
 @Entity
 data class Location(val Name: String, @PrimaryKey val LocationId: String, val FormalName: String, var displayOrder: Int = 0, var isHidden: Boolean = false)
 
