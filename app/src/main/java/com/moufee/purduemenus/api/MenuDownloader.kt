ee0238c8199608dc7ae39cf13531eaebc5d4d71a
Ben Ferris <ben.ferris1@gmail.com>
2019-07-06 17:09:00 -0400

    Allow request failures in MenuDownloader, fix test

--- app/src/main/java/com/moufee/purduemenus/api/MenuDownloader.kt
+++ app/src/main/java/com/moufee/purduemenus/api/MenuDownloader.kt
@@ -3,10 +3,10 @@ package com.moufee.purduemenus.api
 import com.moufee.purduemenus.menus.FullDayMenu
 import com.moufee.purduemenus.menus.Location
 import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.supervisorScope
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -15,14 +15,21 @@ class MenuDownloader @Inject constructor(val webservice: Webservice) {
 
     private val formatter = DateTimeFormat.forPattern("yyyy-MM-dd")
 
-    suspend fun getMenus(date: DateTime, locations: List<Location>): FullDayMenu = coroutineScope {
+    suspend fun getMenus(date: DateTime, locations: List<Location>): FullDayMenu = supervisorScope {
         val locationNames = locations.map { it.Name }
         val deferred = locationNames.map {
             async {
                 webservice.getMenu(it, formatter.print(date))
             }
         }
-        val locationResponses = deferred.awaitAll()
-        FullDayMenu(locationResponses, date)
+        val mapped = deferred.mapNotNull {
+            try {
+                it.await()
+            } catch (t: Throwable) {
+                Timber.e(t, "Network error.")
+                null
+            }
+        }
+        FullDayMenu(mapped, date)
     }
 }
