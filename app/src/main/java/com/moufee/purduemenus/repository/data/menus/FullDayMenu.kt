ed7bb49072324351dd3ffd8a92b7e6a45de2e77a
Ben Ferris <ben.ferris1@gmail.com>
2020-11-01 17:02:35 -0500

    Remove unused files

--- app/src/main/java/com/moufee/purduemenus/repository/data/menus/FullDayMenu.kt
+++
@@ -1,8 +0,0 @@
-package com.moufee.purduemenus.repository.data.menus
-
-import org.joda.time.DateTime
-
-/**
- * Contains all the menus for all campus dining locations for one day
- */
-data class FullDayMenu(val menus: Map<String, DiningCourtMenu>, val date: DateTime, val hasLateLunch: Boolean)
