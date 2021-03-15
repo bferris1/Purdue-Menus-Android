a793b0032b4665b22d47cd31c8572d2408310e8e
Ben Ferris <ben.ferris1@gmail.com>
2021-03-14 22:07:16 -0400

    Add ViewModel test

--- app/src/main/java/com/moufee/purduemenus/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/moufee/purduemenus/ui/login/LoginViewModel.kt
@@ -22,7 +22,7 @@ class LoginViewModel @Inject constructor(private val authenticationRepository: A
             return
         }
         if (password.isNullOrBlank()) {
-            _loginState.value = _loginState.value?.copy(emailError = R.string.error_field_required)
+            _loginState.value = _loginState.value?.copy(passwordError = R.string.error_field_required)
             return
         }
         _loginState.value = LoginState(isLoading = true)
